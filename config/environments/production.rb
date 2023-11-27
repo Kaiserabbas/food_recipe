7946b3d77a0788316b749b0ad8df3cd8bdd6721d
Qaisar <kayser.abbas@gmail.com>
2023-11-27 10:50:08 +0500

    Add localhost to each environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { host: 'http://127.0.0.1:3000', port: 3000 }
+
 end
