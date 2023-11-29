e714ee50aca5d55461345e5ffca4f1c1cff23350
Qaisar <kayser.abbas@gmail.com>
2023-11-29 21:14:14 +0500

    Add foods controller actions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,11 +3,37 @@ class FoodsController < ApplicationController
     @foods = Food.all
   end
 
-  def show; end
+  def show
+    @food = Food.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @food = Food.new
+  end
 
-  def create; end
+  def create
+    @food = current_user.foods.new(food_params)
+    respond_to do |format|
+      if @food.save
+        format.html { redirect_to foods_path, notice: 'Food was created successfully' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  def destroy; end
+  def destroy
+  @food = Food.find(params[:id])
+    @food.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to foods_path, notice: 'Food was deleted successfully' }
+    end         
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :description, :image)
+  end
 end
