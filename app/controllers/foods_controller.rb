98e414feaf19f96455af23539300acf301148183
coker2019 <olabisi2k11@gmail.com>
2023-11-29 09:30:42 +0100

    update controllers files

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,13 @@
 class FoodsController < ApplicationController
-    def index
-      @foods = Food.all
-    end
-  
-    def show; end
-  
-    def new; end
-  
-    def create; end
-  
-    def destroy; end
+  def index
+    @foods = Food.all
   end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def destroy; end
+end
