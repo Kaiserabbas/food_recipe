51017fdd3bbf558678f6b2884328adf647d12e87
Qaisar <kayser.abbas@gmail.com>
2023-11-30 07:34:07 +0500

    Add create method in recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,9 +7,23 @@ class RecipesController < ApplicationController
 
   def new; end
 
-  def create; end
+  def create
+    @recipe = current_user.recipes.new
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to recipes_path, notice: 'Recipe was created successfully' }
+      else
+        format.html do
+          flash[:error] = @recipe.errors.full_messages.join(', ')
+          render :new, status: :unprocessable_entity
+        end
+      end
+    end
+  end
 
   def destroy; end
 
   def update; end
+
+  def edit; end
 end
