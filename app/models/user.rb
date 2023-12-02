c16c54d764b5e8a44316a76e5403bb0885f5a4ca
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-12-02 20:10:31 +0500

    Add self confitmation rule

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,12 @@ class User < ApplicationRecord
   has_many :foods, foreign_key: :user_id, dependent: :destroy
   has_many :recipe_foods, dependent: :destroy
   attr_accessor :confirmation_sent_at
-
+  after_create :skip_confirmation!
   validates :name, presence: true
 
+  def skip_confirmation!
+    self.confirm
+  end
   def admin?
     role == 'admin'
   end
